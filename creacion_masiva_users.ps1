796a7c0f12b862e1382d28d8eafef968d4f4d21e
psantana5 <127206784+psantana5@users.noreply.github.com>
2023-03-22 23:02:00 +0100

    Delete creacion_masiva_users.ps1

--- creacion_masiva_users.ps1
+++
@@ -1,9 +0,0 @@
-﻿#Massive user creation using a CSV file.
-
-$user = Import-Csv -Path C:\Users\%username%\Downloads\usuarios.csv
-foreach ($i in $user)
-{
-    $password = ConvertTo-SecureString $i.contra -AsPlainText -Force
-    New-LocalUser $i.nombre -Password $password -AccountNeverExpires -PasswordNeverExpires
-    Add-LocalGroupMember -group users -Member $i.nombre
-}
