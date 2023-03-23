1c2a5aa5eb67d998b1dc1b07bdca73b7cd532bdb
Pau Santana <127206784+psantana5@users.noreply.github.com>
2023-03-23 11:35:33 +0100

    Add files via upload
    Check for open ports, and detect possible vulnerabilities.

---
+++ Check_Ports.ps1
@@ -0,0 +1,30 @@
+﻿$network = "192.168.0"
+$range = 1..254
+
+foreach ($r in $range) {
+    $ip = "$network.$r"
+
+    # Check if the IP address is reachable
+    if (Test-Connection -Count 1 -ComputerName $ip -Quiet) {
+
+        # Check for open ports using Test-NetConnection cmdlet
+        $portStatus = Test-NetConnection -ComputerName $ip -Port 1-65535 -InformationLevel Quiet
+
+        foreach ($status in $portStatus) {
+            if ($status.TcpTestSucceeded) {
+                Write-Host "$ip port $($status.RemotePort) open"
+            }
+        }
+
+        # Add more vulnerability checks as needed
+
+    } else {
+        Write-Host "$ip is not reachable."
+    }
+}
+
+    if ($openPorts -contains "192.168.0.1:3389") {
+        Write-Host "Recommendation: Consider disabling RDP access from outside the network."
+    }
+
+    # Add more recommendations as needed
